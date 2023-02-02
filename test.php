410fd630cad3690b746ad60a6a6c9657fcff1feb
maharjannivaa <maharjan.niva@webexpertsnepal.com>
2023-02-02 12:05:52 +0545

    new change

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
   <title>PHP-Test</title>
  </head>
  <body>
-  <?php echo "<h1>Hello World (This is new changes to the file)</h1>"
+  <?php echo "<h1>Hello php world (This is new changes to the file)</h1>"
   "<h2>added heading h2</h2>"
   QA branch changes
   ?>
