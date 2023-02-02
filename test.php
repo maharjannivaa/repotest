add9b80846dd628d49e1a713070e4754d27aa9de
maharjannivaa <maharjan.niva@webexpertsnepal.com>
2023-02-02 10:33:11 +0545

    added new text

--- test.php
+++ test.php
@@ -3,7 +3,8 @@
   <title>PHP-Test</title>
  </head>
  <body>
-  <?php echo '<h1>Hello World (This is new changes to the file)</h1>'
+  <?php echo "<h1>Hello World (This is new changes to the file)</h1>"
+  "<h2>added heading h2</h2>"
   ?>
  </body>
 </html>
